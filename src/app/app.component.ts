d26b9b08ce2ab8fc808a46b09e605eaae2557242
diegovsil <diegovsil@inditex.com>
2025-04-17 22:46:58 +0000

    adds basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatList, MatListItem } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, MatList, MatListItem, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'tests', children: [
+            {
+                path: '1',
+                loadComponent: () => import('./workspace/tests/test1/test1.component').then(m => m.Test1Component)
+            },
+            {
+                path: '2',
+                loadComponent: () => import('./workspace/tests/test2/test2.component').then(m => m.Test2Component)
+            },
+        ]
+    }
+];
