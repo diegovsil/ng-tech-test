809a8e546bb24d9475297b0b1dbdb61081fccf77
diegovsil <diegovsil@inditex.com>
2025-04-18 17:43:09 +0000

    adds bitcoin scenario

---
+++ src/app/workspace/tests/test2/services/test2.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { delay, interval, map, Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Test2Service {
+
+  constructor() { }
+
+
+
+  // simulates a delayed http request
+  getBankAccountBalance$(): Observable<number> {
+    return of(5000000).pipe(delay(100));
+  }
+
+  // simulates service that emits bitcoin price every ten seconds
+  getBitcoinPrice$(): Observable<number> {
+    return interval(10000).pipe( // Every second
+      map(() => {
+        // Generate a random number between 40,000 and 60,000
+        return Math.floor(Math.random() * (60000 - 40000 + 1)) + 40000;
+      })
+    );
+  }
+}
--- src/app/workspace/tests/test2/test2.component.ts
+++ src/app/workspace/tests/test2/test2.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Test2Service } from './services/test2.service';
+import { forkJoin, map, Observable, take, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-test2',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, MatButton],
   templateUrl: './test2.component.html',
   styleUrl: './test2.component.scss'
 })
-export class Test2Component {
+export class Test2Component implements OnInit {
+  backAccountBalance$: Observable<number>;
+  backAccountBalance: number;
+  bitcoinPrice$: Observable<number>;
+  bitcoinPrice: number;
+  bitcoinsPurchased$: Observable<number>;
+  constructor(private test2Service: Test2Service) {
 
+  }
+
+  ngOnInit(): void {
+
+
+  }
+
+  getBankAccountBalance() {
+    if (!this.backAccountBalance) {
+      this.backAccountBalance$ = this.test2Service.getBankAccountBalance$();
+    }
+  }
+
+  getBitcoinPrice() {
+    this.bitcoinPrice$ = this.test2Service.getBitcoinPrice$();
+  }
+
+  buyBitcoins() {
+    this.bitcoinsPurchased$ = forkJoin([
+      this.backAccountBalance$,
+      this.bitcoinPrice$
+    ]).pipe(
+      map(([backAccountBalance, bitcoinPrice]) => {
+        return Math.floor(backAccountBalance / bitcoinPrice);
+      })
+    );
+  }
 }
