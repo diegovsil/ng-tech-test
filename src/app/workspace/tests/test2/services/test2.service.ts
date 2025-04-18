c22dd456d67ad4d565df51d9c476a36edf115cc3
diegovsil <diegovsil@inditex.com>
2025-04-18 17:48:31 +0000

    adds ifs clauses

--- src/app/workspace/tests/test2/services/test2.service.ts
+++ src/app/workspace/tests/test2/services/test2.service.ts
@@ -15,9 +15,9 @@ export class Test2Service {
     return of(5000000).pipe(delay(100));
   }
 
-  // simulates service that emits bitcoin price every ten seconds
+  // simulates service that emits bitcoin price every 2 seconds
   getBitcoinPrice$(): Observable<number> {
-    return interval(10000).pipe( // Every second
+    return interval(2000).pipe( // Every second
       map(() => {
         // Generate a random number between 40,000 and 60,000
         return Math.floor(Math.random() * (60000 - 40000 + 1)) + 40000;
