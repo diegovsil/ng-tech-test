ffadcc6c0e764de5b5256a6bd9e0efd17eba92bf
diegovsil <diegovsil@inditex.com>
2025-04-18 00:46:30 +0000

    adds retirement pension

--- src/app/workspace/tests/test1/test1-children/test1-children.component.ts
+++ src/app/workspace/tests/test1/test1-children/test1-children.component.ts
@@ -1,15 +1,28 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-test1-children',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon, MatButton],
   templateUrl: './test1-children.component.html',
   styleUrl: './test1-children.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Test1ChildrenComponent {
-  @Input() person: {name: string, age: number} 
+export class Test1ChildrenComponent implements OnChanges {
+  pension: number = 0;
+  @Input() person: { name: string, age: number }
   retirementAge: number = 67;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.person) {
+      this.getPension();
+    }
+  }
+
+  getPension() {
+    this.pension = Math.round(2000 / this.retirementAge * (this.person.age) * 10) / 10;
+  }
+
 }
