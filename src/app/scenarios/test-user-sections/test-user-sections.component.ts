4b935ade0a5050d73003cb8571d775ab05a137e1
diegovsil <diegovsil@inditex.com>
2025-04-20 21:25:11 +0000

    adds styles

--- src/app/scenarios/test-user-sections/test-user-sections.component.ts
+++ src/app/scenarios/test-user-sections/test-user-sections.component.ts
@@ -3,11 +3,12 @@ import { MatAnchor, MatButton } from '@angular/material/button';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { User, UserType, UserTypes } from './models/user.model';
 import { TestUserSectionsService } from './services/test-user-sections.service';
+import {MatChip, MatChipSet} from '@angular/material/chips';
 
 @Component({
   selector: 'zh-test-test-user-sections',
   standalone: true,
-  imports: [MatButton, RouterLink, RouterOutlet, MatAnchor],
+  imports: [MatButton, RouterLink, RouterOutlet, MatAnchor, MatChip, MatChipSet],
   templateUrl: './test-user-sections.component.html',
   styleUrl: './test-user-sections.component.scss',
 })
@@ -16,15 +17,16 @@ export class TestUserSectionsComponent {
   readonly guestType: UserType = UserTypes.guest;
   user: User;
 
-  constructor(private testUserSectionsService: TestUserSectionsService) {
+  constructor(private testUserSectionsService: TestUserSectionsService,) {
     this.user = this.testUserSectionsService.user;
   }
 
   setRegisteredUser() {
     this.testUserSectionsService.userType = this.registeredType;
+    this.user = this.testUserSectionsService.user;
   }
   setGuestdUser() {
-    this.testUserSectionsService.userType = this.guestType
+    this.testUserSectionsService.userType = this.guestType;
+    this.user = this.testUserSectionsService.user;
   }
-
 }
