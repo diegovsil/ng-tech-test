766ec6813d05156c6ee05c0b3b05dcaf992cedac
Diego <diegovsil@inditex.com>
2025-04-22 15:17:28 +0200

    changes test order

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,22 +6,6 @@ export const routes: Routes = [
     children: [
       {
         path: '1',
-        loadComponent: () => import('./scenarios/test-pension/test-pension.component').then((m) => m.TestPensionComponent),
-      },
-      {
-        path: '2',
-        loadComponent: () => import('./scenarios/test-bitcoin/test-bitcoin.component').then((m) => m.TestBitcoinComponent),
-      },
-      {
-        path: '3',
-        loadComponent: () => import('./scenarios/test-songs-finder/test-songs-finder.component').then((m) => m.TestSongsFinderComponent),
-      },
-      {
-        path: '4',
-        loadComponent: () => import('./scenarios/test-shop/test-shop.component').then((m) => m.TestShopComponent),
-      },
-      {
-        path: '5',
         loadComponent: () => import('./scenarios/test-user-sections/test-user-sections.component').then((m) => m.TestUserSectionsComponent),
         children: [
           {
@@ -31,6 +15,22 @@ export const routes: Routes = [
           },
         ],
 
+      },
+      {
+        path: '2',
+        loadComponent: () => import('./scenarios/test-pension/test-pension.component').then((m) => m.TestPensionComponent),
+      },
+      {
+        path: '3',
+        loadComponent: () => import('./scenarios/test-bitcoin/test-bitcoin.component').then((m) => m.TestBitcoinComponent),
+      },
+      {
+        path: '4',
+        loadComponent: () => import('./scenarios/test-songs-finder/test-songs-finder.component').then((m) => m.TestSongsFinderComponent),
+      },
+      {
+        path: '5',
+        loadComponent: () => import('./scenarios/test-shop/test-shop.component').then((m) => m.TestShopComponent),
       },
     ],
   },
