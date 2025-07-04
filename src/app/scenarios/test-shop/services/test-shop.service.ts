288448278e3fdff7a8edb7a6360362849fca6720
Diego <diegovsil@inditex.com>
2025-07-04 10:33:47 +0200

    feat: implement product listing and cart functionality in test shop component

--- src/app/scenarios/test-shop/services/test-shop.service.ts
+++ src/app/scenarios/test-shop/services/test-shop.service.ts
@@ -12,4 +12,5 @@ export class TestShopService {
   getProducts$(): Observable<Product[]> {
     return of(PRODUCTS);
   }
+
 }
--- src/app/scenarios/test-shop/test-shop.component.ts
+++ src/app/scenarios/test-shop/test-shop.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { Product } from './models/products';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { TestShopService } from './services/test-shop.service';
+import { AsyncPipe, NgIf, NgFor, CommonModule } from '@angular/common';
 @Component({
   selector: 'zh-test-test-shop',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './test-shop.component.html',
   styleUrl: './test-shop.component.scss'
 })
 export class TestShopComponent {
 
+  shopService = inject(TestShopService);
+  products$: Observable<Product[]> = this.shopService.getProducts$();
+  private cartSubject = new BehaviorSubject<Product[]>([])
+  cart$ = this.cartSubject.asObservable();
+
+  addToCart(product: Product): void {
+    const currentCart = this.cartSubject.getValue();
+    this.cartSubject.next([...currentCart, product]);
+  }
+
+  removeFromCart(product: Product): void {
+    const currentCart = this.cartSubject.getValue();
+    const updatedCart = currentCart.filter(p => p.id !== product.id);
+    this.cartSubject.next(updatedCart);
+  }
 }
