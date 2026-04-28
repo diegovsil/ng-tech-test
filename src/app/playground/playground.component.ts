63a46f622abdaaf0c7bd1d0bc52293512b0d6c71
Diego <diegovsil@inditex.com>
2026-04-28 18:32:55 +0200

    feat: implement API simulation with mergeMap in PlaygroundComponent

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { of, from } from 'rxjs';
+import { concatMap, mergeMap, switchMap, delay } from 'rxjs/operators';
 
 @Component({
   selector: 'zh-test-playground',
@@ -7,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrl: './playground.component.scss',
 })
 export class PlaygroundComponent {
+  constructor() {
+    this.clicks.pipe(mergeMap((id) => this.simulateApi(id))).subscribe(console.log);
+    //this.clicks.pipe(concatMap((id) => this.simulateApi(id))).subscribe(console.log);
+    //this.clicks.pipe(switchMap((id) => this.simulateApi(id))).subscribe(console.log);
+  }
 
+  clicks = from(['Petición A', 'Petición B', 'Petición C']);
+
+  // 2. Función que simula una llamada a API
+  simulateApi(name: string) {
+    // La petición A tarda 2s, la B tarda 0.5s y la C tarda 1s
+    const delays: any = { 'Petición A': 2000, 'Petición B': 500, 'Petición C': 1000 };
+
+    console.log(`🚀 Lanzando: ${name} (tardará ${delays[name]}ms)`);
+    return of(`✅ Respuesta de ${name}`).pipe(delay(delays[name]));
+  }
+
+
+  // PRUEBA 1: mergeMap (Paralelo)
+  // Verás que B llega primero porque es la más rápida.
+
+  // PRUEBA 2: concatMap (Fila India)
+  // No empieza B hasta que A termina. Tardan 2s + 0.5s + 1s.
+  // clicks.pipe(concatMap(id => simulateApi(id))).subscribe(console.log);
+
+  // PRUEBA 3: switchMap (El que cancela)
+  // Como los clics son instantáneos, cancelará A y B antes de que terminen.
+  // clicks.pipe(switchMap(id => simulateApi(id))).subscribe(console.log);
 }
