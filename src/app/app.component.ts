eeb620c1f4dcd096341d5e709355370bf008de7d
Diego <diegovsil@inditex.com>
2026-04-28 18:29:14 +0200

    feat: enhance playground link with icon and title in the sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatList, MatListItem } from '@angular/material/list';
+import { MatList, MatListItem, MatListItemIcon, MatListItemTitle } from '@angular/material/list';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, MatSidenavModule, MatList, MatListItem, RouterLink, RouterLinkActive],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [RouterOutlet, MatSidenavModule, MatList, MatListItem, RouterLink, RouterLinkActive, MatIcon, MatListItemIcon, MatListItemTitle],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng-tech-test';
