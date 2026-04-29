7f43356f399c4626b79ba10ea606160eacd45ac3
Diego <diegovsil@inditex.com>
2026-04-29 09:24:52 +0200

    feat: add Child and Parent components with templates, styles, and tests

---
+++ src/app/playground/child/child.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zh-test-child',
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.scss',
+})
+export class ChildComponent {
+
+}
